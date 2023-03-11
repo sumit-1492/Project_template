2e52d53511a8d0b4064e9204ea7f205060b33948
sumit-1492 <sumitlala99@gmail.com>
2023-03-11 20:02:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from typing import List
+
+PROJECT_NAME = "Machine Learning Project"
+VERSION = '0.0.1'
+AUTHOR = "SUMIT KUMAR"
+DESCRIPTION = "ML project"
+
+
+setup(
+    name = PROJECT_NAME,
+    version= VERSION,
+    author = AUTHOR,
+    description= DESCRIPTION,
+    packages= find_packages(), ## main init file our project folder
+    install_requires = 
+)
